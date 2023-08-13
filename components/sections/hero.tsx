1e23271e891591568e09d6cbaf9eed8178182831
Ramon Blanquer <blanquer.ramon@gmail.com>
2023-08-13 13:34:33 +0100

    Cloth on hero.

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -30,10 +30,9 @@ const Hero = forwardRef<HTMLHeadingElement>(function Hero({}, forwardedRef) {
                 <h1 ref={forwardedRef} className="text-center text-[80px] sm:text-[70px] leading-none font-title">
                   Ramon Blanquer
                 </h1>
-                <h1 className="text-center text-[60px] leading-none font-serif ">
-                  Playfair Display
-                </h1>
-                <div className="grow"><Cloth /></div>
+                <div className="grow">
+                  <Cloth />
+                </div>
               </div>
             </section>
           </div>
