b35a0b12a26bd5f18ed343721da838fce19f5df5
Ramon Blanquer <blanquer.ramon@gmail.com>
2023-09-02 19:28:07 +0300

    Worked on hero page decoration.

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -20,6 +20,10 @@ const Hero = forwardRef<HTMLHeadingElement>(function Hero({}, forwardedRef) {
     console.log("[Hero] Rendering");
   }
 
+  // TODO: Do animation of the scrollYProgress bar and then swap it with the scrollYProgress.
+  // TODO: Automatically scroll a little?
+  // TODO: LFO gravity?
+
   return (
     <>
       <div>
@@ -31,15 +35,129 @@ const Hero = forwardRef<HTMLHeadingElement>(function Hero({}, forwardedRef) {
                   className="h-[20px] bg-black"
                   style={{ scaleX: scrollYProgress }}
                 />
-                <h1
-                  ref={forwardedRef}
-                  className="text-center text-[80px] sm:text-[70px] leading-none font-title"
+                <motion.div
+                  className="h-g2 flex justify-center overflow-visible"
+                  initial="hidden"
+                  animate="visible"
+                  variants={{
+                    visible: {
+                      opacity: 1,
+                      transition: {
+                        duration: 0.001,
+                        delayChildren: 0,
+                        staggerChildren: 0.05,
+                      },
+                    },
+                    hidden: {
+                      opacity: 0,
+                    },
+                  }}
                 >
-                  Ramon Blanquer
-                </h1>
+                  {[...Array(40)].map((e, i) => (
+                    <motion.div
+                      key={i}
+                      className="min-w-g2 bg-white border-2 border-black -mr-[2px]"
+                      variants={{
+                        visible: { opacity: 1, x: 0, y: 0 },
+                        hidden: { opacity: 0, x: -40, y: -40 },
+                      }}
+                    ></motion.div>
+                  ))}
+                </motion.div>
+                <motion.div
+                  className="h-g5 flex justify-center overflow-visible  -mt-[2px]"
+                  initial="hidden"
+                  animate="visible"
+                  variants={{
+                    visible: {
+                      opacity: 1,
+                      transition: {
+                        duration: 0.001,
+                        delayChildren: 0,
+                        staggerChildren: 0.1,
+                      },
+                    },
+                    hidden: {
+                      opacity: 0,
+                    },
+                  }}
+                >
+                  {[...Array(8)].map((e, i) => (
+                    <motion.div
+                      key={i}
+                      className="min-w-g5 bg-white border-2 border-black -mr-[2px]"
+                      variants={{
+                        visible: { opacity: 1, y: 0 },
+                        hidden: { opacity: 0, y: -40 },
+                      }}
+                    ></motion.div>
+                  ))}
+                  <motion.div
+                    className="min-w-[666px] border-2 border-black -mr-[2px] flex flex-col items-center justify-center"
+                    variants={{
+                      visible: { opacity: 1, y: 0 },
+                      hidden: { opacity: 0, y: -40 },
+                    }}
+                  >
+                    <h1
+                      ref={forwardedRef}
+                      className="text-center text-[80px] sm:text-[70px] leading-none font-title"
+                    >
+                      Ramon Blanquer
+                    </h1>
+                    <h2 className="text-center text-[28px] sm:text-[20px] leading-none font-mono">
+                      from code to deployment; from back&nbsp;&nbsp;to front;
+                    </h2>
+                  </motion.div>
+                  {[...Array(8)].map((e, i) => (
+                    <motion.div
+                      key={i}
+                      className="min-w-g5 border-2 border-black -mr-[2px]"
+                      variants={{
+                        visible: { opacity: 1, y: 0 },
+                        hidden: { opacity: 0, y: -40 },
+                      }}
+                    ></motion.div>
+                  ))}
+                </motion.div>
+
                 <div className="grow overflow-hidden">
-                  {environment.disableGraphics ? "" : <Cloth scrollYProgress={scrollYProgress} />}
+                  {environment.disableGraphics ? (
+                    ""
+                  ) : (
+                    <Cloth scrollYProgress={scrollYProgress} />
+                  )}
                 </div>
+
+                <motion.div
+                  className="h-g3 flex justify-center overflow-visible -mb-[2px]"
+                  initial="hidden"
+                  animate="visible"
+                  variants={{
+                    visible: {
+                      opacity: 1,
+                      transition: {
+                        duration: 0.001,
+                        staggerChildren: 0.05,
+                        staggerDirection: -1,
+                      },
+                    },
+                    hidden: {
+                      opacity: 0,
+                    },
+                  }}
+                >
+                  {[...Array(40)].map((e, i) => (
+                    <motion.div
+                      key={i}
+                      className="min-w-g3 bg-white border-2 border-black -mr-[2px]"
+                      variants={{
+                        visible: { opacity: 1, x: 0, y: 0 },
+                        hidden: { opacity: 0, x: -40, y: -40 },
+                      }}
+                    ></motion.div>
+                  ))}
+                </motion.div>
               </div>
             </section>
           </div>
