4fa9c8489e213927aeddd194be4dd6ca961b1e1d
Ramon Blanquer <blanquer.ramon@gmail.com>
2023-10-21 16:42:53 +0200

    Lighter font on the recommendations.

--- components/recommendation.tsx
+++ components/recommendation.tsx
@@ -3,5 +3,5 @@ import { FunctionComponent, ReactNode } from "react";
 export const Recommendation: FunctionComponent<{ children: ReactNode }> = ({
   children,
 }) => {
-  return <blockquote className="italic indent-8">{children}</blockquote>;
+  return <blockquote className="italic font-light indent-8">{children}</blockquote>;
 };
