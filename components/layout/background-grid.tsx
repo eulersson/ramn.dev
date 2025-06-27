830d4cb81b9c2c85cae2401f429c7b835a4acf33
Ramon Blanquer <blanquer.ramon@gmail.com>
2025-06-27 18:52:12 +0200

    chore(bg-grid): refactored css into tailwind

--- components/layout/background-grid.tsx
+++ components/layout/background-grid.tsx
@@ -1,52 +1,61 @@
 "use client";
 
 // Third-Party
-import { memo, useState } from "react";
+import { memo, useRef, useState } from "react";
+import { usePathname } from "next/navigation";
 
 // Project
+import { useBreakpoint, useBreakpointChange } from "@/hooks/breakpoint";
 import { toBool } from "@/lib";
 
 // Styles
-import { useBreakpoint, useBreakpointChange } from "@/hooks/breakpoint";
 import "./background-grid.css";
+
+// Configuration for different routes
+const GRID_CONFIG = {
+  "/": {
+    "2xl": 112,
+    xl: 112,
+    lg: 124,
+    md: 132,
+    sm: 140,
+    xs: 192,
+    xxs: 332,
+  },
+  "/work": {
+    "2xl": 44,
+    xl: 40,
+    lg: 44,
+    md: 44,
+    sm: 48,
+    xs: 48,
+    xxs: 104,
+  },
+} as const;
+
+const PLATE_GRID_ITEM_CLASSES =
+  "bg-back transition-[border-radius] hover:rounded-[40px] duration-500 ease-in-out";
+
+const DEBUG_CLASSES =
+  "w-[4px] h-[2px] mt-[calc((var(--bg-grid-box-size)/2)+1px)] -translate-[2px] bg-red-500";
 
 export const BackgroundGrid = memo(function BackgroundGrid() {
   const debugGrid = toBool(process.env.NEXT_PUBLIC_DEBUG_GRID);
-
+  const bgGridRef = useRef<HTMLDivElement>(null);
+  const pathname = usePathname();
   const [numBoxes, setNumBoxes] = useState(128);
-
   const bpXs = useBreakpoint("xs");
 
   useBreakpointChange((bp) => {
-    switch (bp) {
-      case "2xl": {
-        setNumBoxes(112);
-        break;
-      }
-      case "xl": {
-        setNumBoxes(112);
-        break;
-      }
-      case "lg": {
-        setNumBoxes(120);
-        break;
-      }
-      case "md": {
-        setNumBoxes(132);
-        break;
-      }
-      case "sm": {
-        setNumBoxes(140);
-        break;
-      }
-      case "xs": {
-        setNumBoxes(192);
-        break;
-      }
-    }
-
+    const routeConfig =
+      GRID_CONFIG[pathname as keyof typeof GRID_CONFIG] || GRID_CONFIG["/"];
     if (bpXs.isSmaller) {
-      setNumBoxes(332);
+      setNumBoxes(routeConfig["xxs"]);
+    } else {
+      const numBoxesForBreakpoint = routeConfig[bp as keyof typeof routeConfig];
+      if (numBoxesForBreakpoint) {
+        setNumBoxes(numBoxesForBreakpoint);
+      }
     }
   });
 
@@ -55,7 +64,7 @@ export const BackgroundGrid = memo(function BackgroundGrid() {
   }
 
   return (
-    <div className="w-full flex">
+    <div ref={bgGridRef} className="w-full flex">
       {/* Left half */}
       <div
         className={`plate-grid ml-px justify-end ${
@@ -63,10 +72,8 @@ export const BackgroundGrid = memo(function BackgroundGrid() {
         }`}
       >
         {[...Array(numBoxes)].map((_, i) => (
-          <div className="plate-grid--item bg-back" key={i}>
-            {debugGrid && (
-              <div className="w-[4px] h-[2px] mt-[calc((var(--bg-grid-box-size)/2)+1px)] -translate-[2px] bg-red-500"></div>
-            )}
+          <div className={PLATE_GRID_ITEM_CLASSES} key={i}>
+            {debugGrid && <div className={DEBUG_CLASSES}></div>}
           </div>
         ))}
       </div>
@@ -77,10 +84,8 @@ export const BackgroundGrid = memo(function BackgroundGrid() {
         }`}
       >
         {[...Array(numBoxes)].map((_, i) => (
-          <div className="plate-grid--item bg-back" key={i}>
-            {debugGrid && (
-              <div className="w-[4px] h-[2px] mt-[calc((var(--bg-grid-box-size)/2)+1px)] -translate-[2px] bg-red-500"></div>
-            )}
+          <div className={PLATE_GRID_ITEM_CLASSES} key={i}>
+            {debugGrid && <div className={DEBUG_CLASSES}></div>}
           </div>
         ))}
       </div>
